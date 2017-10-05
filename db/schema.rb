d74c6e4b3284e29117669ebdc91c7fd1ef22ff7e
Chris Philbin <chris@chrisphilbin.net>
2017-10-05 23:47:51 +0000

    Finish user edit, update, index, and destroy action

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925224038) do
+ActiveRecord::Schema.define(version: 20171003224203) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'booleen' for column 'admin'
 
 end
